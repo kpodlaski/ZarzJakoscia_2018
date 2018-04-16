38dd4d89219671322415553bacb5d58097e8e2d1
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2018-04-16 23:19:14 +0200

    diagramy

---
+++ zaoczneAI/Mockito/src/main/java/app/ClassToTest.java
@@ -0,0 +1,7 @@
+package app;
+
+/**
+ * Created by Krzysztof Podlaski on 17.03.2018.
+ */
+public class ClassToTest {
+}
---
+++ zaoczneAI/Mockito/src/test/java/app/ClassToTestTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Krzysztof Podlaski on 17.03.2018.
+ */
+public class ClassToTestTest {
+
+}
